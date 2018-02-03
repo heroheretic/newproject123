912caa2fa8c34fe6d8054cae9b242f0d29822e6e
heroheretic <heroheretic@gmail.com>
2018-02-03 19:43:35 +0700

    adfads

--- app/src/main/java/com/boss/duc/myapplication/MainActivity.java
+++ app/src/main/java/com/boss/duc/myapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package com.boss.duc.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bt1,bt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt1=(Button)findViewById(R.id.bt1);
+        bt2=(Button)findViewById(R.id.bt2);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Branch2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
